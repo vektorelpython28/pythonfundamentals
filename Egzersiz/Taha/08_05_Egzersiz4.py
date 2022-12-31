29fafa7ddb680a67603232728a690dc2ea00f309
vektorelpython28 <vektorelpython28@gmail.com>
2022-12-31 09:20:00 +0000

    31122022_5

---
+++ Egzersiz/Taha/08_05_Egzersiz4.py
@@ -0,0 +1,5 @@
+""" Girilen bir sayıyı sözlü olarak ekrana yazdıran python
+    programını yazınız. Örn:
+    3,249,245 sayısını 
+    üç milyon iki yüz kırk dokuz bin iki yüz kırk beş olarak ifade etmelidir.
+    """
--- olusturma.py
+++ olusturma.py
@@ -1,15 +1,16 @@
 liste = ["Tuna","Aydın","Ceren",
-    "Özlem","Bora","Emre","Muhammed","Naz","Ahmet","Muruvet","Taha","Iclal","Nur","Akın","Busra"]
+    "Özlem","Bora","Emre","Muhammed","Naz","Ahmet","Muruvet","Taha","Iclal","Nur","Akın","Busra","Ediz"]
 import os 
-dosyaismi = "08_05_Egzersiz3"
+dosyaismi = "08_05_Egzersiz4"
 folderName = "Egzersiz"
 for item in liste:
     if not os.path.exists(os.path.join(folderName,item)):
         os.mkdir(os.path.join(folderName,item))
     dosya = open(fr"/workspace/pythonfundamentals/Egzersiz/{item}/{dosyaismi}.py","a+")
-    metin = """\"\"\"Taş Kağıt Makas Oyununu kullanıcıyla birlikte oynayan bir
-        python programı yazınız. Bu egzersiz sırasında birden fazla
-        fonksiyon ve random modülünü kullanabilirsiniz.
+    metin = """\"\"\" Girilen bir sayıyı sözlü olarak ekrana yazdıran python
+    programını yazınız. Örn:
+    3,249,245 sayısını 
+    üç milyon iki yüz kırk dokuz bin iki yüz kırk beş olarak ifade etmelidir.
     \"\"\"
 """
     dosya.write(metin)
