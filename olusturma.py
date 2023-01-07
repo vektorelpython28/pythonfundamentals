4da775c6fe79a9c06fd7e9b1ca2783d0a6bdb78f
vektorelpython28 <vektorelpython28@gmail.com>
2023-01-07 06:45:02 +0000

    07012023

--- olusturma.py
+++ olusturma.py
@@ -1,7 +1,7 @@
 liste = ["Tuna","Aydın","Ceren",
     "Özlem","Bora","Emre","Muhammed","Naz","Ahmet","Muruvet","Taha","Iclal","Nur","Akın","Busra","Ediz"]
 import os 
-dosyaismi = "08_05_Egzersiz4"
+dosyaismi = "10_01_TemelKavramlar"
 folderName = "Egzersiz"
 for item in liste:
     if not os.path.exists(os.path.join(folderName,item)):
