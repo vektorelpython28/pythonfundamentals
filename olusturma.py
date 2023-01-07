654aa2d91d53587e49a8879837250d287a5dd694
vektorelpython28 <vektorelpython28@gmail.com>
2023-01-07 06:45:52 +0000

    07012023

--- olusturma.py
+++ olusturma.py
@@ -6,12 +6,4 @@ folderName = "Egzersiz"
 for item in liste:
     if not os.path.exists(os.path.join(folderName,item)):
         os.mkdir(os.path.join(folderName,item))
-    dosya = open(fr"/workspace/pythonfundamentals/Egzersiz/{item}/{dosyaismi}.py","a+")
-    metin = """\"\"\" Girilen bir sayıyı sözlü olarak ekrana yazdıran python
-    programını yazınız. Örn:
-    3,249,245 sayısını 
-    üç milyon iki yüz kırk dokuz bin iki yüz kırk beş olarak ifade etmelidir.
-    \"\"\"
-"""
-    dosya.write(metin)
-    dosya.close()
+    dosya = open(fr"/workspace/pythonfundamentals/Egzersiz/{item}/{dosyaismi}.py","w+")
