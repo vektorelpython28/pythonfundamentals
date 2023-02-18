e6bc12162cdb89b6b0434e5a03b101756be1fdc6
vektorelpython28 <vektorelpython28@gmail.com>
2023-02-18 08:39:22 +0000

    18022023_1

--- olusturma.py
+++ olusturma.py
@@ -1,9 +1,9 @@
 liste = ["Tuna","Aydın","Ceren",
     "Özlem","Bora","Emre","Muhammed","Naz","Ahmet","Muruvet","Taha","Iclal","Nur","Akın","Busra","Ediz"]
 import os 
-dosyaismi = "11_02_Egzersiz"
+dosyaismi = "12_01_Temel"
 folderName = "Egzersiz"
 for item in liste:
     if not os.path.exists(os.path.join(folderName,item)):
         os.mkdir(os.path.join(folderName,item))
-    dosya = open(fr"/workspace/pythonfundamentals/Egzersiz/{item}/{dosyaismi}.py","w+")
+    dosya = open(fr"/workspace/pythonfundamentals/Egzersiz/{item}/{dosyaismi}.ipynb","w+")
